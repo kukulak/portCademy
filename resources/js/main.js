a56d239bf3cee4e7d52d1b0327bccbf218427f1f
Christian Valderrama <kukulak.cero@gmail.com>
2022-01-06 10:20:24 -0600

    menu mobile

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,6 +6,15 @@ const about = document.getElementById('about')
 const projects = document.getElementById('projects')
 const skills = document.getElementById('skils')
 const contact = document.getElementById('contact')
+const menuMobile = document.getElementsByClassName('menuMobile')
+const hamMenu = document.getElementsByClassName('hamMenu')
+
+const hamAnimation = (element) => {
+    element.style.top === '0px' ? element.style.top = '-500px' : element.style.top = '0px'
+    return true
+}
+
+hamMenu[0].addEventListener('click', function(){hamAnimation(menuMobile[0])})
 
 let menu = document.getElementsByClassName('menuItem')
 console.log(menu)
@@ -13,7 +22,7 @@ let menuArray = Array.from(menu)
 
 const menuChanges = () => {
     menuArray.forEach(element => {
-        document.getElementById(element.innerHTML.toLowerCase()).style.gridRowStart = 1
+        document.getElementById(element.innerHTML.toLowerCase()).style.gridRowStart = '1'
         element.onclick = menuChanges()
     });
 }
