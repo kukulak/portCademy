638892938e62fe66be672bcfcf93bc5237e8084c
Christian Valderrama <kukulak.cero@gmail.com>
2022-01-06 07:42:40 -0600

    font Gx instaled

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,8 +2,24 @@ console.log('hell to start')
 
 const imgMorris = document.getElementById('imgMorris')
 const imgAhorra = document.getElementById('imgAhorra')
+const about = document.getElementById('about')
+const projects = document.getElementById('projects')
+const skills = document.getElementById('skils')
+const contact = document.getElementById('contact')
 
-console.log(imgMorris)
+let menu = document.getElementsByClassName('menuItem')
+console.log(menu)
+let menuArray = Array.from(menu)
+
+const menuChanges = () => {
+    menuArray.forEach(element => {
+        document.getElementById(element.innerHTML.toLowerCase()).style.gridRowStart = 1
+        element.onclick = menuChanges()
+    });
+}
+
+
+
 imgMorris.onmouseover = function(event){imgMorris.style.opacity='0.02'} 
 imgMorris.onmouseout = function(event){imgMorris.style.opacity='1'} 
 
@@ -11,4 +27,3 @@ imgAhorra.onmouseover = function(event){imgAhorra.style.opacity='0.02'}
 imgAhorra.onmouseout = function(event){imgAhorra.style.opacity='1'} 
 
 
-// imgMorris.onmouseover = function(event){imgMorris.style.marginTop='100px'} 
